da8d3d16999093a65a200296b2ff6b90c168ab68
sarooo17 <130158714+sarooo17@users.noreply.github.com>
2024-04-19 00:08:48 +0200

    inizializzazione css globale + navbar backend

---
+++ php/backend.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+    <title>Backend</title>
+    <link rel="stylesheet" type="text/css" href="../css/AllAroundF1.css">
+    <link rel="stylesheet" type="text/css" href="../css/backend.css">
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <a href="backend.php"><img src="../img/logo/white-horse.svg" alt=""></a>
+        </div>
+        <div class="links">
+            <ul>
+                <li>
+                    <a href="#" class="red-text">
+                        <i class="bi bi-house"></i>
+                        <span class="nav-text">Dashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="bi bi-gear"></i>
+                        <span class="nav-text">Factory</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="bi bi-box"></i>
+                        <span class="nav-text">Logistics</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="bi bi-people"></i>
+                        <span class="nav-text">Staff</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="bi bi-currency-exchange"></i>
+                        <span class="nav-text">Finances</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="bi bi-megaphone"></i>
+                        <span class="nav-text">Marketing</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="bi bi-calendar-week"></i>
+                        <span class="nav-text">Calendar</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="account">
+            <ul>
+                <li>
+                    <a href="#">
+                        <i class="bi bi-person"></i>
+                        <span class="nav-text">Account</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
