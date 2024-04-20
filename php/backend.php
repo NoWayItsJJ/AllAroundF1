bc82090dfd5f60996cc47dffee1b9629c62ce950
sarooo17 <rickysaro17@gmail.com>
2024-04-20 11:58:32 +0200

    started calendar developing

--- php/backend.php
+++ php/backend.php
@@ -5,6 +5,8 @@
 
     <link rel="stylesheet" type="text/css" href="../css/AllAroundF1.css">
     <link rel="stylesheet" type="text/css" href="../css/backend.css">
+
+    <script src="../js/calendar.js"></script>
 </head>
 <body>
     <nav>
@@ -76,6 +78,19 @@
                     <h3>Calendar</h3>
                 </div>
                 <div class="card-content">
+                    <div class="calendar" onload="startTime()">
+                        <div class="calendar-header">
+                            <div class="calendar-header-month">
+                                <h4 id="month"></h4>
+                            </div>
+                            <div class="calendar-header-nav">
+                                <button class="button-invisible-background" onclick="prevMonth()"><i class="bi bi-arrow-left"></i></button>
+                                <button class="button-invisible-background" onclick="today()">today</button>
+                                <button class="button-invisible-background" onclick="nextMonth()"><i class="bi bi-arrow-right"></i></button>
+                            </div>
+                        </div>
+                        <div class="calendar-content"></div>
+                    </div>
                 </div>
                 <div class="card-footer">
                     <div class="card-link">
@@ -163,4 +178,89 @@
         </div>
     </section>    
 </body>
+<script>
+    function startTime() {
+	var weekday = new Array();
+	weekday[0] = "Sunday";
+	weekday[1] = "Monday";
+	weekday[2] = "Tuesday";
+	weekday[3] = "Wednesday";
+	weekday[4] = "Thursday";
+	weekday[5] = "Friday";
+	weekday[6] = "Saturday";
+	var month = new Array();
+	month[0] = "January";
+	month[1] = "February";
+	month[2] = "March";
+	month[3] = "April";
+	month[4] = "May";
+	month[5] = "June";
+	month[6] = "July";
+	month[7] = "August";
+	month[8] = "September";
+	month[9] = "October";
+	month[10] = "November";
+	month[11] = "December";
+	var today = new Date();
+	var h = today.getHours();
+	var m = today.getMinutes();
+	var s = today.getSeconds();
+	var d = today.getDate();
+	var y = today.getFullYear();
+	var wd = weekday[today.getDay()];
+	var mt = month[today.getMonth()];
+
+	m = checkTime(m);
+	s = checkTime(s);
+	document.getElementById("date").innerHTML = d;
+	document.getElementById("day").innerHTML = wd;
+	document.getElementById("month").innerHTML = mt + "/" + y;
+
+	var t = setTimeout(startTime, 500);
+}
+
+function prevMonth() {
+	var today = new Date();
+	var m = today.getMonth();
+	var y = today.getFullYear();
+	if (m == 0) {
+		m = 11;
+		y = y - 1;
+	} else {
+		m = m - 1;
+	}
+	var mt = month[m];
+	document.getElementById("month").innerHTML = mt + "/" + y;
+}
+
+function nextMonth() {
+	var today = new Date();
+	var m = today.getMonth();
+	var y = today.getFullYear();
+	if (m == 11) {
+		m = 0;
+		y = y + 1;
+	} else {
+		m = m + 1;
+	}
+	var mt = month[m];
+	document.getElementById("month").innerHTML = mt + " " + y;
+}
+
+function today() {
+	var today = new Date();
+	var m = today.getMonth();
+	var y = today.getFullYear();
+	var mt = month[m];
+	document.getElementById("month").innerHTML = mt + " " + y;
+}
+
+function checkTime(i) {
+	if (i < 10) {
+		i = "0" + i;
+	}
+	return i;
+}
+
+</script>
 </html>
