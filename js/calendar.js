ccd5eecf9b039bb0cde6f1e4f3571a5892bf4125
sarooo17 <rickysaro17@gmail.com>
2024-04-20 13:02:54 +0200

    almost-ended-calendar

--- js/calendar.js
+++ js/calendar.js
@@ -1,82 +1,92 @@
-function startTime() {
-	var weekday = new Array();
-	weekday[0] = "Sunday";
-	weekday[1] = "Monday";
-	weekday[2] = "Tuesday";
-	weekday[3] = "Wednesday";
-	weekday[4] = "Thursday";
-	weekday[5] = "Friday";
-	weekday[6] = "Saturday";
-	var month = new Array();
-	month[0] = "January";
-	month[1] = "February";
-	month[2] = "March";
-	month[3] = "April";
-	month[4] = "May";
-	month[5] = "June";
-	month[6] = "July";
-	month[7] = "August";
-	month[8] = "September";
-	month[9] = "October";
-	month[10] = "November";
-	month[11] = "December";
+var date = new Date();
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var dayNames = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+function calendar() {
 	var today = new Date();
-	var h = today.getHours();
-	var m = today.getMinutes();
-	var s = today.getSeconds();
-	var d = today.getDate();
-	var y = today.getFullYear();
-	var wd = weekday[today.getDay()];
-	var mt = month[today.getMonth()];
-
-	m = checkTime(m);
-	s = checkTime(s);
-	document.getElementById("date").innerHTML = d;
-	document.getElementById("day").innerHTML = wd;
-	document.getElementById("month").innerHTML = mt + "/" + y;
-
-	var t = setTimeout(startTime, 500);
+	var currentDay = today.getDate();
+	var month = date.getMonth();
+	var year = date.getFullYear();
+	var firstDay = new Date(year, month, 1);
+	var startingDay = firstDay.getDay();
+	var monthLength = daysInMonth(month, year);
+	var monthName = months[month];
+	document.getElementById("month").innerHTML = monthName + " " + year;
+	var html = "<table class='calendar-content'>";
+	html += "<tr>";
+	for (var i = 0; i <= 6; i++) {
+		html += "<th>";
+		html += dayNames[i];
+		html += "</th>";
+	}
+	html += "</tr><tr>";
+	var day = 1;
+	startingDay = startingDay === 0 ? 7 : startingDay;
+	for (var i = 0; i < 9; i++) {
+		for (var j = 1; j <= 7; j++) {
+			html += "<td>";
+			if (day <= monthLength && (i > 0 || j >= startingDay)) {
+				if (day === currentDay && month === today.getMonth() && year === today.getFullYear()) {
+					html += "<span class='current-day'>";
+				}
+				html += day;
+				if (day === currentDay && month === today.getMonth() && year === today.getFullYear()) {
+					html += "</span>";
+				}
+				day++;
+			}
+			html += "</td>";
+		}
+		if (day > monthLength) {
+			break;
+		} else {
+			html += "</tr><tr>";
+		}
+	}
+	html += "</tr></table>";
+	document.getElementById("calendar").innerHTML = html;
 }
 
 function prevMonth() {
-	var today = new Date();
-	var m = today.getMonth();
-	var y = today.getFullYear();
+	var m = date.getMonth();
+	var y = date.getFullYear();
 	if (m == 0) {
 		m = 11;
 		y = y - 1;
 	} else {
 		m = m - 1;
 	}
-	var mt = month[m];
-	document.getElementById("month").innerHTML = mt + "/" + y;
+	date.setMonth(m);
+	date.setFullYear(y);
+	document.getElementById("month").innerHTML = months[m] + " " + y;
+	calendar();
 }
 
 function nextMonth() {
-	var today = new Date();
-	var m = today.getMonth();
-	var y = today.getFullYear();
+	var m = date.getMonth();
+	var y = date.getFullYear();
 	if (m == 11) {
 		m = 0;
 		y = y + 1;
 	} else {
 		m = m + 1;
 	}
-	var mt = month[m];
-	document.getElementById("month").innerHTML = mt + " " + y;
+	date.setMonth(m);
+	date.setFullYear(y);
+	document.getElementById("month").innerHTML = months[m] + " " + y;
+	calendar();
 }
 
-function today() {
+function displayToday() {
 	var today = new Date();
 	var m = today.getMonth();
 	var y = today.getFullYear();
-	var mt = month[m];
-	document.getElementById("month").innerHTML = mt + " " + y;
+	date.setMonth(m);
+	date.setFullYear(y);
+	document.getElementById("month").innerHTML = months[m] + " " + y;
+	calendar();
 }
 
-function checkTime(i) {
-	if (i < 10) {
-		i = "0" + i;
-	}
-	return i;
+function daysInMonth(month, year) {
+	return new Date(year, month + 1, 0).getDate();
 }
