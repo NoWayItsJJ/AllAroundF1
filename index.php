b58e72021fd3bb6ab3cc8549caa412883ace637f
NoWayItsJJ <fabio.pauletta@gmail.com>
2024-04-26 19:55:50 +0200

    added sign up link button

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <html>
     <body>
         <button onclick="location.href='./php/login.php'">Login</button>
+        <button onclick="location.href='./php/signin.php'">Sign Up</button>
     </body>
 </html>
