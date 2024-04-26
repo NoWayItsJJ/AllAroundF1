b534a653d34c3dd48fb1511f262f11ac2b25716b
sarooo17 <130158714+sarooo17@users.noreply.github.com>
2024-04-26 18:07:13 +0200

    dashboard routing

--- php/backend.php
+++ php/backend.php
@@ -1,16 +1,4 @@
-<?php
-    session_start();
-    if (isset($_SESSION['user_type'])) {
-        $user_type = $_SESSION['user_type'];
-        if ($user_type == 5) {
-            header('Location: access_denied.php');
-            exit();
-        }
-    } else {
-        header('Location: ../php/login.php');
-        exit();
-    }
-?>
+<?php include 'security.php'; ?>
 <html>
 <head>
     <title>Ferrari Backend</title>
@@ -52,7 +40,7 @@
     <section id="dashboard" class="scrollable-section">
         <div class="dashboard-grid">
             <div class="dashboard-card red grid-col-span-2">
-                <div class="card-header">
+                <div class="card-header" onclick="window.location.href='calendar.php';">
                     <i class="bi bi-calendar-week"></i>
                     <h4>Calendar</h4>
                 </div>
@@ -83,13 +71,13 @@
                     </div>
                 </div>
                 <div class="card-footer">
-                    <div class="card-link">
+                    <div class="card-link" onclick="window.location.href='calendar.php';">
                         <i class="bi bi-arrow-down-right"></i>
                     </div>
                 </div>
             </div>
             <div class="dashboard-card">
-                <div class="card-header">
+                <div class="card-header" onclick="window.location.href='finances.php';">
                     <i class="bi bi-currency-exchange"></i>
                     <h4>Finances</h4>
                 </div>
@@ -101,7 +89,7 @@
                     </div>
                 </div>
             </div>
-            <div class="dashboard-card grid-row-span-2">
+            <div class="dashboard-card grid-row-span-2" onclick="window.location.href='staff.php';">
                 <div class="card-header">
                     <i class="bi bi-people"></i>
                     <h4>Staff</h4>
@@ -109,58 +97,58 @@
                 <div class="card-content">
                 </div>
                 <div class="card-footer">
-                    <div class="card-link">
+                    <div class="card-link" onclick="window.location.href='finances.php';">
                         <i class="bi bi-arrow-down-right"></i>
                     </div>
                 </div>
             </div>
             <div class="dashboard-card grid-row-span-2">
-                <div class="card-header">
+                <div class="card-header"  onclick="window.location.href='logistics.php';">
                     <i class="bi bi-box"></i>
                     <h4>Logistics</h4>
                 </div>
                 <div class="card-content">
                 </div>
                 <div class="card-footer">
-                    <div class="card-link">
+                    <div class="card-link"  onclick="window.location.href='logistics.php';">
                         <i class="bi bi-arrow-down-right"></i>
                     </div>
                 </div>
             </div>
             <div class="dashboard-card grid-col-span-2">
-                <div class="card-header">
+                <div class="card-header"  onclick="window.location.href='factory.php';">
                     <i class="bi bi-gear"></i>
                     <h4>Factory</h4>
                 </div>
                 <div class="card-content">
                 </div>
                 <div class="card-footer">
-                    <div class="card-link">
+                    <div class="card-link" onclick="window.location.href='factory.php';">
                         <i class="bi bi-arrow-down-right"></i>
                     </div>
                 </div>
             </div>
             <div class="dashboard-card">
-                <div class="card-header">
+                <div class="card-header" onclick="window.location.href='marketing.php';">
                     <i class="bi bi-megaphone"></i>
                     <h4>Marketing</h4>
                 </div>
                 <div class="card-content">
                 </div>
                 <div class="card-footer">
-                    <div class="card-link">
+                    <div class="card-link" onclick="window.location.href='marketing.php';">
                         <i class="bi bi-arrow-down-right"></i>
                     </div>
                 </div>
             </div>
             <div class="dashboard-card grid-col-span-2">
-                <div class="card-header">
+                <div class="card-header" onclick="window.location.href='drivers.php';">
                     <h4>Drivers</h4>
                 </div>
                 <div class="card-content">
                 </div>
                 <div class="card-footer">
-                    <div class="card-link">
+                    <div class="card-link" onclick="window.location.href='drivers.php';">
                         <i class="bi bi-arrow-down-right"></i>
                     </div>
                 </div>
