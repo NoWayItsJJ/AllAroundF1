52a567caadeb9768752c225eb6885433af99e107
sarooo17 <rickysaro17@gmail.com>
2024-04-27 12:55:45 +0200

    started staff development

--- php/staff.php
+++ php/staff.php
@@ -6,6 +6,7 @@
 
     <link rel="stylesheet" type="text/css" href="../css/AllAroundF1.css">
     <link rel="stylesheet" type="text/css" href="../css/backend.css">
+    <link rel="stylesheet" type="text/css" href="../css/dynamic-dashboard.css">
 </head>
 <body>
     <nav>
@@ -29,11 +30,43 @@
         </div>
     </nav>
     <section id="dynamic-dashboard">
-        <div class="dynamic-dashbord-grid">
-            <div class="dynamic-dashbord-card"></div>
-            <div class="dynamic-dashbord-card"></div>
-            <div class="dynamic-dashbord-card"></div>
-            <div class="dynamic-dashbord-card"></div>
+        <div class="dynamic-dashboard-grid">
+            <div class="dynamic-dashboard-card grid-col-span-4"></div>
+            <div class="dynamic-dashboard-card">
+                <div class="new-button">
+                    <i class="bi bi-plus"></i>
+                    <h3>New</h3>
+                </div>
+            </div>
+            <div class="dynamic-dashboard-card grid-col-span-4 grid-row-span-5">
+                <div class="list">
+                    <div class="list-header">
+                        <h3>Staff</h3>
+                        <div class="search">
+                            <input type="text" placeholder="Search">
+                            <i class="bi bi-search"></i>
+                        </div>
+                    </div>
+                    <div class="list-content">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Staff ID</th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Position</th>
+                                    <th>Salary</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="dynamic-dashboard-card grid-row-span-5"></div>
         </div>
     </section>
 </body>
