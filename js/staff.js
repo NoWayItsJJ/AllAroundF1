15d0a96cf2d3844da063e7fa402e5691cb9c416c
NoWayItsJJ <fabio.pauletta@gmail.com>
2024-05-04 10:34:12 +0200

    completed search for staff

--- js/staff.js
+++ js/staff.js
@@ -1,71 +1,68 @@
-$(document).ready(function() {
-    $('.staff-list-row').click(function(e) {
-        e.preventDefault();
-        var staffId = $(this).children().first().data('id');
+$(document).ready(function () {
+	$(".staff-list-row").click(function (e) {
+		e.preventDefault();
+		var staffId = $(this).children().first().data("id");
 
-        $.ajax({
-            type: 'POST',
-            url: '../php/staff-function.php',
-            data: { 
-                id: staffId, 
-                action: 'getDetails' 
-            },
-            dataType: 'json',
-            success: function (response) {
-                if (response.getDetails) {
-                    console.log(response.details);
-                    
-                }
-                else {
-                    console.log('Error');
-                }
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-                console.error('Error:', textStatus, errorThrown);
-                console.error('Response:', jqXHR.responseText);
-            }
-        });
-        
-    });
+		$.ajax({
+			type: "POST",
+			url: "../php/staff-function.php",
+			data: {
+				id: staffId,
+				action: "getDetails",
+			},
+			dataType: "json",
+			success: function (response) {
+				if (response.getDetails) {
+					console.log(response.details);
+				} else {
+					console.log("Error");
+				}
+			},
+			error: function (jqXHR, textStatus, errorThrown) {
+				console.error("Error:", textStatus, errorThrown);
+				console.error("Response:", jqXHR.responseText);
+			},
+		});
+	});
 
-    $('.statistic').first().find('.statistic-icon').addClass('active');
+	$(".statistic").first().find(".statistic-icon").addClass("active");
 
-    $('.statistic').click(function() {
-        var roleId = $(this).data('role-id');
+	$(".statistic").click(function () {
+		var search = $("#search").val();
+		var roleId = $(this).data("role-id");
 
-        $('.statistic-icon').each(function() {
-            $(this).removeClass('active');
-        });
+		$(".statistic-icon").each(function () {
+			$(this).removeClass("active");
+		});
 
-        $(this).find('.statistic-icon').each(function() {
-            $(this).addClass('active');
-        });
+		$(this)
+			.find(".statistic-icon")
+			.each(function () {
+				$(this).addClass("active");
+			});
 
-        $.ajax({
-            url: 'staff-list.php',
-            type: 'POST',
-            data: { 'fk_id_ruolo': roleId },
-            success: function(response) {
-                $('#list-result').html(response);
-            }
-        });
-    });
+		$.ajax({
+			url: "staff-list.php",
+			type: "POST",
+			data: { search: search, fk_id_ruolo: roleId },
+			success: function (response) {
+				$("#list-result").html(response);
+			},
+		});
+	});
 
-    $('#search').keyup(function() {
-        var search = $(this).val();
-        var roleId = $('.statistic-icon.active').parent().data('role-id');
-        console.log(roleId);
+	$("#search").keyup(function () {
+		var search = $(this).val();
+		var roleId = $(".statistic-icon.active").parent().data("role-id");
 
-        $.ajax({
-            url: 'staff-list.php',
-            type: 'POST',
-            data: { 'search': search,
-                    'fk_id_ruolo': roleId
-             },
-            success: function (response) {
-                console.log(response);
-                $('#list-result').html(response);
-            }
-        });
-    });
+		$.ajax({
+			url: "staff-list.php",
+			type: "POST",
+			data: { search: search, fk_id_ruolo: roleId },
+			success: function (response) {
+				console.log(response);
+				$("#list-result").html(response);
+			},
+		});
+	});
 });
