452771c70bb8d0c65e47c57fbf9937e5499828fd
sarooo17 <rickysaro17@gmail.com>
2024-05-04 12:25:23 +0200

    organization account-backend js

--- php/account-backend.php
+++ php/account-backend.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" type="text/css" href="../css/account-backend.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="../js/account-backend.js"></script>
 </head>
 <body>
     <nav>
@@ -230,93 +231,4 @@
         <a href="./logout.php" class="exit" role="button"><strong>Logout</strong></a>
     </section>
 </body>
-<script>
-function toggleForm(formId, iconId) {
-    // Seleziona il form e l'icona specifici
-    var form = document.getElementById(formId);
-    var icon = document.getElementById(iconId);
-
-    // Se il form specifico è già aperto...
-    if (form.classList.contains('active')) {
-        // Chiudi il form specifico e ruota l'icona specifica nella posizione originale
-        form.classList.remove('active');
-        icon.classList.remove('rotate');
-    } else {
-        // Chiudi tutti i form
-        var forms = document.querySelectorAll('form');
-        forms.forEach(function(form) {
-            if (form.classList.contains('active')) {
-                form.classList.remove('active');
-            }
-        });
-
-        // Ruota tutte le icone nella posizione originale
-        var icons = document.querySelectorAll('.bi-chevron-right');
-        icons.forEach(function(icon) {
-            if (icon.classList.contains('rotate')) {
-                icon.classList.remove('rotate');
-            }
-        });
-
-        // Apri il form specifico e ruota l'icona specifica
-        form.classList.add('active');
-        icon.classList.add('rotate');
-    }
-}
-
-function clearInput(id) {
-    var input = document.getElementById(id);
-    input.value = '';
-}
-
-function openPopup() {
-    var popup = document.getElementById('screen-overlay');
-    popup.classList.add('open-overlay');
-}
-
-function closePopup() {
-    document.getElementById('screen-overlay').classList.remove('open-overlay');
-    document.getElementById('file').value = '';
-    var preview = document.getElementById('preview');
-    var filenameElement = document.getElementById('filename');
-    
-    preview.src = '';
-    filenameElement.textContent = '';
-
-    preview.style.display = 'none';
-    filenameElement.style.display = 'none';
-}
-
-$(document).ready(function() {
-    $('#confirmPassword').on('input', function() {
-        if ($('#confirmPassword').val() == '') {
-            $('#confirmPassword').removeClass('valid invalid');
-        } else if ($('#inputPassword').val() != $('#confirmPassword').val()) {
-            $('#confirmPassword').removeClass('valid');
-            $('#confirmPassword').addClass('invalid');
-        } else {
-            $('#confirmPassword').removeClass('invalid');
-            $('#confirmPassword').addClass('valid');
-        }
-    });
-    
-});
-
-document.getElementById('file').addEventListener('change', function(e) {
-    var fileName = e.target.files[0].name;
-    var reader = new FileReader();
-    reader.onload = function(e) {
-        var preview = document.getElementById('preview');
-        var filenameElement = document.getElementById('filename');
-        
-        preview.src = e.target.result;
-        filenameElement.textContent = fileName;
-
-        // Mostra gli elementi
-        preview.style.display = 'block';
-        filenameElement.style.display = 'block';
-    };
-    reader.readAsDataURL(e.target.files[0]);
-});
-</script>
 </html>
