e324b3940d1dc185837a64e73c0b149ade1a46da
NoWayItsJJ <fabio.pauletta@gmail.com>
2024-05-16 21:41:25 +0200

    almost ended logistics (filters missing)

--- php/logistics.php
+++ php/logistics.php
@@ -6,6 +6,11 @@
 
     <link rel="stylesheet" type="text/css" href="../css/AllAroundF1.css">
     <link rel="stylesheet" type="text/css" href="../css/backend.css">
+    <link rel="stylesheet" type="text/css" href="../css/dynamic-dashboard.css">
+    <link rel="stylesheet" type="text/css" href="../css/staff.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="../js/logistics.js"></script>
 </head>
 <body>
     <nav>
@@ -34,6 +39,116 @@
             </ul>
         </div>
     </nav>
-    
+    <div id="screen-overlay" class="screen-overlay">
+        <div id="popup" class="popup">
+            <div class="popup-header">
+            <span class="popup-title"></span>
+                <i class="bi bi-x" onclick="closePopup()"></i>
+            </div>
+            <div id="popup-content" class="popup-content">
+            </div>
+        </div>
+    </div>
+    <section id="dynamic-dashboard">
+        <div class="dynamic-dashboard-grid">
+            <div class="dynamic-dashboard-card grid-col-span-4">
+                <div class="card-row">
+                <?php 
+                    include('logistics_count.php'); 
+                ?>
+                </div>
+            </div>
+            <div class="dynamic-dashboard-card popup-open buttonNew" data-form-type="newForm" data-header="New employee">
+                <div class="new-button">
+                    <i class="bi bi-plus"></i>
+                    <h3>New</h3>
+                </div>
+            </div> 
+            <div class="dynamic-dashboard-card grid-col-span-4 grid-row-span-5">
+                <div class="list">
+                    <div class="list-header">
+                        <h3>Staff</h3>
+                        <div class="search">
+                            <input type="text" id="search" placeholder="Search">                            
+                            <i class="bi bi-search"></i>
+                        </div>
+                    </div>
+                    <div class="line"></div>
+                    <div class="list-content">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Moved</th>
+                                    <th>From</th>
+                                    <th>To</th>
+                                    <th>Departure date</th>
+                                    <th>Arrival date</th>
+                                </tr>
+                            </thead>
+                            <tbody id="list-result">
+                                <?php include 'logistics-list.php'; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="dynamic-dashboard-card grid-row-span-5">
+                <div class="details" id="detailsBlock" style="display: none;">
+                    <div class="details-header">
+                        <div class="item-img">
+                            <img id="userImage" src="" alt="">
+                        </div>
+                        <div class="item-header-info">
+                            <h3 id="userName"></h3>
+                            <p id="userRole"></p>
+                        </div>
+                    </div>
+                    <div class="details-content">
+                        <h4>Personal Info</h4>
+                        <div id="personal-info">
+                            <div class="personal-info-row">
+                                <p id="userId" hidden></p>
+                                <p id="roleId" hidden></p>
+                                <p id="age"><strong>Age</strong></p>
+                                <p id="displayAge"></p>
+                            </div>
+                            <div class="personal-info-row">
+                                <p id="nationality"><strong>Nationality</strong></p>
+                                <p id="displayNationality"></p>
+                            </div>
+                            <div class="personal-info-row">
+                                <p id="email"><strong>Email</strong></p>
+                                <p id="displayEmail"></p>
+                            </div>
+                            <div class="personal-info-row">
+                                <p id="specialization"><strong>Specialization</strong></p>
+                                <p id="displaySpecialization"></p>
+                            </div>
+                        </div>
+                        <h4>Contract Info</h4>
+                        <div id="contract-info">
+                            <div class="contract-info-row">
+                                <p id="salary"><strong>Salary</strong></p>
+                                <p id="displaySalary"></p>
+                            </div>
+                            <div class="contract-info-row">
+                                <p id="end"><strong>Contract end</strong></p>
+                                <p id="displayEnd"></p>
+                            </div>
+                            <div class="contract-info-row">
+                                <p id="bonus"><strong>Bonus</strong></p>
+                                <p id="displayBonus"></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="details-footer">
+                        <button class="button-primary red-button button-max-width popup-open" data-form-type="fireForm" data-header="Fire employee">Fire</button>
+                        <button class="button-primary green-button button-max-width popup-open" data-form-type="renewForm" data-header="Renew contract">Renew</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
+
 </html>
