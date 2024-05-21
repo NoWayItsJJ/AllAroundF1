df4069992206eaffe24b98a6a9e7da2d2953d049
sarooo17 <rickysaro17@gmail.com>
2024-05-21 12:59:00 +0200

    set up factory and marketing pages

--- php/factory.php
+++ php/factory.php
@@ -6,6 +6,8 @@
 
     <link rel="stylesheet" type="text/css" href="../css/AllAroundF1.css">
     <link rel="stylesheet" type="text/css" href="../css/backend.css">
+    <link rel="stylesheet" type="text/css" href="../css/dynamic-dashboard.css">
+    <link rel="stylesheet" type="text/css" href="../css/marketing.css">
 </head>
 <body>
     <nav>
@@ -34,6 +36,117 @@
             </ul>
         </div>
     </nav>
-    
+    <section id="dynamic-dashboard">
+        <div class="dynamic-dashboard-grid marketing"> 
+            <div class="dynamic-dashboard-card grid-col-span-3 grid-row-span-3">
+                <div class="list">
+                    <div class="list-header">
+                        <div class="title">
+                            <div class="title-text">
+                                <i class="bi bi-people"></i>
+                                <h4>Car</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="list-content">
+                    </div>
+                    <div class="list-footer">
+                        <div class="list-link" onclick="window.location.href='events.php';">
+                            <i class="bi bi-arrow-down-right"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="dynamic-dashboard-card grid-col-span-3 grid-row-span-5">
+                <div class="list">
+                    <div class="list-header">
+                        <div class="title">
+                            <div class="title-text">
+                                <i class="bi bi-people"></i>
+                                <h4>Components</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="list-card">
+                        <div class="filter-card">
+                            <div class="card-row">
+                                <?php //include('merchandise_count.php'); ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="list-content">
+                        <div class="table-header">
+                            <p>Name</p>
+                            <p>Email</p>
+                            <p>Position</p>
+                            <p>Specialization</p>
+                        </div>
+                        <div id="list-result" class="table-body scrollable-section">
+                            <?php //include 'merchandise-list.php'; ?> <!-- sarebbe da fare con meno campi -->
+                        </div>
+                    </div>
+                    <div class="list-footer">
+                        <div class="list-link" onclick="window.location.href='merchandise.php';">
+                            <i class="bi bi-arrow-down-right"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="dynamic-dashboard-card grid-col-span-3 grid-row-span-5">
+                <div class="list">
+                    <div class="list-header">
+                        <div class="title">
+                            <div class="title-text">
+                                <i class="bi bi-people"></i>
+                                <h4>Factory Engineers</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="list-card">
+                        <div class="filter-card">
+                            <div class="card-row">
+                                <?php //include('order_count.php'); ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="list-content">
+                        <div class="table-header">
+                            <p>Name</p>
+                            <p>Email</p>
+                            <p>Position</p>
+                            <p>Specialization</p>
+                        </div>
+                        <div id="list-result" class="table-body scrollable-section">
+                            <?php //include 'order-list.php'; ?> <!-- sarebbe da fare con meno campi -->
+                        </div>
+                    </div>
+                    <div class="list-footer">
+                        <div class="list-link" onclick="window.location.href='order.php';">
+                            <i class="bi bi-arrow-down-right"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="dynamic-dashboard-card grid-col-span-3 grid-row-span-3">
+                <div class="list">
+                    <div class="list-header">
+                        <div class="title">
+                            <div class="title-text">
+                                <i class="bi bi-people"></i>
+                                <h4>Production</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="list-content">
+                    </div>
+                    <div class="list-footer">
+                        <div class="list-link" onclick="window.location.href='sponsor.php';">
+                            <i class="bi bi-arrow-down-right"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
--- php/marketing.php
+++ php/marketing.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" type="text/css" href="../css/AllAroundF1.css">
     <link rel="stylesheet" type="text/css" href="../css/backend.css">
     <link rel="stylesheet" type="text/css" href="../css/dynamic-dashboard.css">
+    <link rel="stylesheet" type="text/css" href="../css/marketing.css">
 </head>
 <body>
     <nav>
@@ -38,12 +39,112 @@
     <section id="dynamic-dashboard">
         <div class="dynamic-dashboard-grid marketing"> 
             <div class="dynamic-dashboard-card grid-col-span-3 grid-row-span-6">
+                <div class="list">
+                    <div class="list-header">
+                        <div class="title">
+                            <div class="title-text">
+                                <i class="bi bi-people"></i>
+                                <h4>Merchandise</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="list-card">
+                        <div class="filter-card">
+                            <div class="card-row">
+                                <?php //include('merchandise_count.php'); ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="list-content">
+                        <div class="table-header">
+                            <p>Name</p>
+                            <p>Email</p>
+                            <p>Position</p>
+                            <p>Specialization</p>
+                        </div>
+                        <div id="list-result" class="table-body scrollable-section">
+                            <?php //include 'merchandise-list.php'; ?> <!-- sarebbe da fare con meno campi -->
+                        </div>
+                    </div>
+                    <div class="list-footer">
+                        <div class="list-link" onclick="window.location.href='merchandise.php';">
+                            <i class="bi bi-arrow-down-right"></i>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="dynamic-dashboard-card grid-col-span-3 grid-row-span-2">
+                <div class="list">
+                    <div class="list-header">
+                        <div class="title">
+                            <div class="title-text">
+                                <i class="bi bi-people"></i>
+                                <h4>Events</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="list-content">
+                    </div>
+                    <div class="list-footer">
+                        <div class="list-link" onclick="window.location.href='events.php';">
+                            <i class="bi bi-arrow-down-right"></i>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="dynamic-dashboard-card grid-col-span-3 grid-row-span-6">
+                <div class="list">
+                    <div class="list-header">
+                        <div class="title">
+                            <div class="title-text">
+                                <i class="bi bi-people"></i>
+                                <h4>Orders</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="list-card">
+                        <div class="filter-card">
+                            <div class="card-row">
+                                <?php //include('order_count.php'); ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="list-content">
+                        <div class="table-header">
+                            <p>Name</p>
+                            <p>Email</p>
+                            <p>Position</p>
+                            <p>Specialization</p>
+                        </div>
+                        <div id="list-result" class="table-body scrollable-section">
+                            <?php //include 'order-list.php'; ?> <!-- sarebbe da fare con meno campi -->
+                        </div>
+                    </div>
+                    <div class="list-footer">
+                        <div class="list-link" onclick="window.location.href='order.php';">
+                            <i class="bi bi-arrow-down-right"></i>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="dynamic-dashboard-card grid-col-span-3 grid-row-span-2">
+                <div class="list">
+                    <div class="list-header">
+                        <div class="title">
+                            <div class="title-text">
+                                <i class="bi bi-people"></i>
+                                <h4>Sponsor</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="list-content">
+                    </div>
+                    <div class="list-footer">
+                        <div class="list-link" onclick="window.location.href='sponsor.php';">
+                            <i class="bi bi-arrow-down-right"></i>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
